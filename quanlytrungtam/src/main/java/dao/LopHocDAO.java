7cd067c696c0eff21bba4acefa9d708de91c69d9
ka khuyen <kakhuyen2446@gmail.com>
2026-04-15 22:49:04 +0700

    add LopHocDAO

---
+++ quanlytrungtam/src/main/java/dao/LopHocDAO.java
@@ -0,0 +1,29 @@
+package dao;
+import util.DBConnection;
+import java.sql.*;
+import java.util.*;
+
+public class LopHocDAO {
+    public List<LopHoc> getAll() {
+        List<LopHoc> list = new ArrayList<>();
+        String sql = "SELECT l.*, k.tenKH FROM LOPHOC l JOIN KHOAHOC k ON l.maKH = k.maKH";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                LopHoc l = new LopHoc();
+                l.setMaLH(rs.getInt("maLH"));
+                l.setTenLH(rs.getString("tenLH"));
+                l.setSiSoMax(rs.getInt("siSoMax"));
+                l.setNgayKhaiGiang(rs.getDate("ngayKhaiGiang").toLocalDate());
+                l.setTenKH(rs.getString("tenKH"));
+                list.add(l);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
