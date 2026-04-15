4cb6359cdad9c792ffc91ab9d8e89b00bb63b50e
ka khuyen <kakhuyen2446@gmail.com>
2026-04-15 22:05:17 +0700

    init project

---
+++ quanlytrungtam/src/main/java/util/DBConnection.java
@@ -0,0 +1,21 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class DBConnection {
+    public static Connection conn;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            if (conn == null || conn.isClosed()) {
+                System.out.println("Connecting...");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quanlytrungtam", "root", "root");
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+}
