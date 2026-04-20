e785dec02fecce2f76f0a6dd3c560edd53b775ff
ka khuyen <kakhuyen2446@gmail.com>
2026-04-20 11:40:42 +0700

    sửa lỗi file DBConnection

--- quanlytrungtam/src/main/java/util/DBConnection.java
+++ quanlytrungtam/src/main/java/util/DBConnection.java
@@ -3,12 +3,6 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class DBConnection {
-    public static Connection conn;
-
 public class DBConnection {
 
     public static Connection getConnection() {
